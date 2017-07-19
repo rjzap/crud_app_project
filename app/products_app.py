65b456a5ca0209d7754ed1009e0bb0a36a38363c
rjzap <rick.j.zappala@gmail.com>
2017-07-19 19:39:07 -0400

    perform data lookup for show option

--- app/products_app.py
+++ app/products_app.py
@@ -4,14 +4,14 @@ import getpass #from stackoverflow thread
 uid = getpass.getuser() #from stackoverflow thread
 products = "C:\users\zappari\desktop\python_class\crud_app\data\products.csv"
 
-def lookup(pid):
-    return
-
 with open(products, "r") as csv_file:
     reader = csv.DictReader(csv_file)
     data = list(reader)
     row_count = len(data)
 
+
+def show_lookup(show_param):
+    return [p for p in data if p['id'] == show_param]
 
 print "\n", "Welcome", uid, "\n"
 
@@ -28,7 +28,7 @@ Destroy           |   Delete an existing product
 
 """
 
-user_input = raw_input("Operation Selection:")
+user_input = raw_input("Operation Selection:").title()
 
 if user_input.title() == "List":
     for row in data:
@@ -38,6 +38,19 @@ if user_input.title() == "List":
         #print row
 elif user_input == "Show":
     show_param = raw_input("Please input the ID number for the product you'd like to review:")
+    show_display = show_lookup(show_param)
+    print " + Prodcut ID: ", show_display[0]['id']
+    print " + Name: ", show_display[0]['name']
+    print " + Department: ", show_display[0]['department']
+    print " + Aisle: ", show_display[0]['aisle']
+    print " + Price: ", '${0:.2f}'.format(float(show_display[0]['price']))
+elif user_input == "Create":
+    print "Add a new product"
+elif user_input == "Update":
+    print "Edit an existing product"
+elif user_input == "Destroy":
+    print "Delete an existing product"
+
 
 #menu = {}
 #menu['1']="List"
