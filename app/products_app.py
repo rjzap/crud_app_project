d7ed32b1be827bbd6ddf515f130f653cba6fcafa
rjzap <rick.j.zappala@gmail.com>
2017-07-19 20:00:45 -0400

    updated lookup for show option

--- app/products_app.py
+++ app/products_app.py
@@ -15,7 +15,7 @@ def show_lookup(show_param):
 
 print "\n", "Welcome", uid, "\n"
 
-print "There are ", row_count, " products in the database.  Please select an operation:\n"
+print "There are {0} products in the database.  Please select an operation:\n".format(row_count)
 
 print """
 operation         |   description
@@ -33,23 +33,22 @@ user_input = raw_input("Operation Selection:").title()
 if user_input.title() == "List":
     for row in data:
         print " + ", row["id"], row["name"], row["price"], row["department"]
-        #for key, value in row.iteritems() :
-            #print key, value
-        #print row
 elif user_input == "Show":
     show_param = raw_input("Please input the ID number for the product you'd like to review:")
-    show_display = show_lookup(show_param)
-    print " + Prodcut ID: ", show_display[0]['id']
-    print " + Name: ", show_display[0]['name']
-    print " + Department: ", show_display[0]['department']
-    print " + Aisle: ", show_display[0]['aisle']
-    print " + Price: ", '${0:.2f}'.format(float(show_display[0]['price']))
+    show_display = show_lookup(show_param)[0]
+    print " + Prodcut ID: ", show_display['id']
+    print " + Name: ", show_display['name']
+    print " + Department: ", show_display['department']
+    print " + Aisle: ", show_display['aisle']
+    print " + Price: ", '${0:.2f}'.format(float(show_display['price']))
 elif user_input == "Create":
     print "Add a new product"
 elif user_input == "Update":
     print "Edit an existing product"
 elif user_input == "Destroy":
     print "Delete an existing product"
+else:
+    print "Please type a recognized operation from the menu options"
 
 
 #menu = {}
