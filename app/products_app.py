6dfedcb2296d0cc1158b887750a94a16f0f127df
rjzap <rick.j.zappala@gmail.com>
2017-07-23 14:51:19 -0400

    cleaned up code for more consistent structure, removed while loop

--- app/products_app.py
+++ app/products_app.py
@@ -18,11 +18,23 @@ with open(csv_file_path, "r") as csv_file:
 
 row_count = len(products)
 
-def show_lookup(show_param):
-    return [p for p in products if p['id'] == show_param]
+def prod_list():
+    for row in products:
+        disp_price = '${0:.2f}'.format(float(row['price']))
+        print " + ", row["id"], "|", row["name"], "|", row["department"], "|", row["aisle"], "|", disp_price
+
+def show_lookup():
+    show_param = raw_input("\nPlease input the product ID for the product record you'd like to review: ")
+    prod_lkup = products
+    prod_lkup = [p for p in products if p['id'] == show_param]
+    show_dis = prod_lkup[0]
+    show_display = od((k, show_dis[k]) for k in disp_key_order)
+    print ""
+    for k, v in show_display.iteritems():
+        print " + ", k.title(), ": ", v
 
 def create_prod():
-    print "To create a new product, please specify the product's information..."
+    print "\nTo create a new product, please specify the product's information...\n"
     product_id = int(products[-1]["id"]) + 1
     product_name = raw_input("Product Name is:")
     product_dep = raw_input("Product department is:")
@@ -35,30 +47,30 @@ def create_prod():
         "aisle" : product_aisle,
         "price" : product_price
         }
-    print "The new product is:\n", new_product
+    print "\nThe new product is:\n", new_product
     products.append(new_product)
 
-def update_prod(upd_param):
+def update_prod():
+    upd_param = raw_input("\nPlease specify the product ID for the product record you'd like to edit:")
     upd_ind = map(itemgetter("id"), products).index(upd_param)
     for k in disp_key_order[1:]:
-        products[upd_ind][k] = raw_input("Change product " + k + " from " + ' "{}"'.format(products[upd_ind][k]) + " to: ")
+        products[upd_ind][k] = raw_input("\nChange product " + k + " from " + ' "{}"'.format(products[upd_ind][k]) + " to: ")
 
 def destroy_prod():
     global products
-    destr_param = raw_input("Please specify the product ID for the record you'd like to delete:")
+    destr_param = raw_input("\nPlease specify the product ID for the record you'd like to delete:")
     prod_elim = [p for p in products if p['id'] == destr_param]
     prod_elim = od((k,prod_elim[0][k]) for k in disp_key_order)
-    print "Destroying: \n"
+    print "\nDestroying: \n"
     for k,v in prod_elim.iteritems():
         print " + ", k.title(), ": ", v
     products = [p for p in products if p['id'] != destr_param]
-
 
 print "\n", "Welcome", uid, "\n"
 
 print "There are {0} products in the database.  Please select an operation:\n".format(row_count)
 
-print """
+menu = """
 operation         |   description
 ---------------   |   ---------------
 List              |   Display a list of product identifiers and names
@@ -66,65 +78,27 @@ Show              |   Show information about a product
 Create            |   Add a new product
 Update            |   Edit an existing product
 Destroy           |   Delete an existing product
-Exit              |   Exit the program
-
 """
+print menu
 
-
-disp_price = '${0:.2f}'.format(float(row['price']))
 disp_key_order = ("id", "name", "department", "aisle", "price")
+select_op = raw_input("\nType desired operation from menu list options:").title()
 
-while True:
-    select_op = raw_input("\nOperation Selection:").title()
-    if select_op == "Exit":
-        break
-    elif select_op == "List":
-        for row in products:
-            print " + ", row["id"], row["name"], row["department"] , row["aisle"], disp_price
-    elif select_op == "Show":
-        show_param = raw_input("Please input the product ID for the product record like to review:")
-        show_dis = show_lookup(show_param)[0]
-        show_display = od((k, show_dis[k]) for k in disp_key_order)
-        for k, v in show_display.iteritems():
-            print " + ", k.title(), ": ", v
-    elif select_op == "Create":
-        create_prod()
-    elif select_op == "Update":
-        upd_param = raw_input("Please specify the product ID for the product record you'd like to edit:")
-        update_prod(upd_param)
-    elif select_op == "Destroy":
-        destroy_prod()
-    else:
-        print "Please input a recognized operation from the menu"
-
+if select_op == "List":
+    prod_list()
+elif select_op == "Show":
+    show_lookup()
+elif select_op == "Create":
+    create_prod()
+elif select_op == "Update":
+    update_prod()
+elif select_op == "Destroy":
+    destroy_prod()
+else:
+    print "Please input a recognized operation from the menu"
 
 with open(csv_file_path, "wb") as csv_file:
-  writer = csv.DictWriter(csv_file, fieldnames=["id","name","aisle","department","price"])
-  writer.writeheader() # uses fieldnames set above
-  for product in products:
-    writer.writerow(product)
-
-
-#menu = {}
-#menu['1']="List"
-#menu['2']="Show"
-#menu['3']="Create"
-#menu['4']="Update"
-#menu['5']="Destroy"
-#while True:
-#  options=menu.keys()
-#  options.sort()
-#    for entry in options:
-#      print entry, menu[entry]
-
-#    selection=raw_input("Please Select:")
-#    if selection =='1':
-#      print "add"
-#    elif selection == '2':
-#      print "delete"
-#    elif selection == '3':
-#      print "find"
-#    elif selection == '4':
-#      break
-#    else:
-#      print "Unknown Option Selected!"
+    writer = csv.DictWriter(csv_file, fieldnames=["id","name","aisle","department","price"])
+    writer.writeheader() # uses fieldnames set above
+    for product in products:
+        writer.writerow(product)
